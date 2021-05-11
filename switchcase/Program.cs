1a5bff25aa207de4c486096404e653a092ea5a5b
karimdevwm <ka.trabelsi9@gmail.com>
2021-05-11 19:25:18 +0200

    Ajoutez des fichiers projet.

---
+++ switchcase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saisir h pour homme ou f pour femme : ");
+            string genre = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            switch (genre)
+            {
+                case("h"):
+                    if (age >= 18)
+                    {
+                        Console.WriteLine("homme");
+                    }
+                    else
+                    {
+                        Console.WriteLine("garçon");
+                    }
+                    break;
+                case("f"):
+                    if (age >= 18)
+                    {
+                        Console.WriteLine("femme");
+                    }
+                    else
+                    {
+                        Console.WriteLine("fille");
+                    }
+                    break;
+            }
+        }
+    }
+}
